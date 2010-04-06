895a00c091c70ea45e2494da104d950c7ea91a6d
John Barton (joho) <jrbarton@gmail.com>
2010-04-06 21:18:20 +1000

    actually get this displaying something

--- shirts.rb
+++ shirts.rb
@@ -1,7 +1,23 @@
 require 'rubygems'
 require 'haml'
 require 'sinatra'
+require 'flickraw'
+
+flickr_config = if File.exists?('flickr.yml')
+  YAML.load_file('flickr.yml')
+else
+  { "secret" => ENV['flickr_secret'],
+    "key" => ENV['flickr_key'] }
+end
 
 get '/' do
+  @photo = flickr.photos.search  :api_key => flickr_config['key'],
+                                :user_id => '86448492@N00',
+                                # :tags => 'everydamnedshirt',
+                                :per_page => 1
+                                 
+  @photo_info = flickr.photos.getInfo(:photo_id => @photo.first['id'])
+  @photo_url = FlickRaw.url(@photo_info)
+  
   haml :index
 end
